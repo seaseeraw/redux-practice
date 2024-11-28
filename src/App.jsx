83ed3f604dc47dabcdd28ca949973689a709e980
seaseeraw <seaseeraw12@gmail.com>
2024-11-28 12:09:37 +1100

    practice redux

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset } from './redux/counterSlice';
+import './App.css';
+
+function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <h1>Redux Counter</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
